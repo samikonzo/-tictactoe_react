e8f655294c00ef722e1d29c34870fdfe1d4b8012
Sami Konzo <samikonzo@gmail.com>
2017-12-18 08:30:24 +0300

    commit

--- src/server.js
+++ src/server.js
@@ -6,24 +6,6 @@ const reload		= require('reload')
 const PORT 			= process.env.PORT || 3000
 const l 			= console.log
 
-/*var gameRooms = {
-	rooms : {},
-	addRoom : function(room){
-		var id = room.id
-		this.rooms[id] = room;
-
-		var link = '/invite_' + id
-
-		app.get(link, (req, res) => {
-			res.sendFile(__dirname + 'index.html')
-		})
-	},
-	removeRoom : function(roomID){
-		delete this.rooms[roomID]
-	}
-}*/
-
-
 /*
 * browser reloader / just for dev
 */
